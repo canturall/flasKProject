5544edec93264a40525ddb6f6e7e2e0b7e62acd7
stj.mctural <stj.mctural@havelsan.com.tr>
2023-07-26 16:05:02 +0300

    update table

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask, jsonify, request, render_template, Response
 import threading
 import time
+from ObjectsInZone import *
 
 app = Flask(__name__)
 
 # Sayacı temsil edecek değişken
 counter = 0
+
 
 def increase_counter():
     global counter
@@ -17,15 +19,18 @@ def increase_counter():
             counter = 0
         time.sleep(1)  # 1 saniye bekle
 
+
 # Arka planda sayacı artıran görevi başlat
 counter_thread = threading.Thread(target=increase_counter)
 counter_thread.daemon = True
 counter_thread.start()
 
+
 @app.route("/")
 def baslangic_api():
     # index.html dosyasını render ederken sayaç değerini template'e gönderiyoruz.
     return render_template("index.html", my_variable=counter)
+
 
 @app.route("/stream")
 def stream():
@@ -37,6 +42,7 @@ def stream():
             time.sleep(1)
 
     return Response(generate(), content_type='text/event-stream')
+
 
 if __name__ == "__main__":
     app.run()
